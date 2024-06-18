cd469d9ba95b404dc37b86b5376a0c60de4224cf
Milhouse-1906 <gabriellopesgoncalves189@gmail.com>
2024-06-18 13:33:28 -0300

    Generate getters setters

--- src/main/java/com/loja/backend/entity/Estado.java
+++ src/main/java/com/loja/backend/entity/Estado.java
@@ -9,11 +9,11 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
-import lombok.Data;
+
 
 @Entity
 @Table(name = "estado")
-@Data
+
 public class Estado {
 
     @Id
@@ -26,4 +26,36 @@ public class Estado {
     private Date dataCriacao;
     @Temporal(TemporalType.TIMESTAMP)
     private Date dataAtualizacao;
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSigla() {
+		return sigla;
+	}
+	public void setSigla(String sigla) {
+		this.sigla = sigla;
+	}
+	public Date getDataCriacao() {
+		return dataCriacao;
+	}
+	public void setDataCriacao(Date dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
+	public Date getDataAtualizacao() {
+		return dataAtualizacao;
+	}
+	public void setDataAtualizacao(Date dataAtualizacao) {
+		this.dataAtualizacao = dataAtualizacao;
+	}
 }
