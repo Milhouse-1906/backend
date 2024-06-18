096f66ec0e16b393b445e20d7a2b0228a74764bd
Milhouse-1906 <gabriellopesgoncalves189@gmail.com>
2024-06-18 14:35:08 -0300

    Crud de mensagem

---
+++ src/main/java/com/loja/backend/entity/Cidade.java
@@ -0,0 +1,72 @@
+package com.loja.backend.entity;
+
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+@Entity
+@Table(name = "cidade")
+
+public class Cidade {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	private String nome;
+	@ManyToOne
+	@JoinColumn(name = "idEstado")
+	private Estado estado;
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date dataCriacao;
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date dataAtualizacao;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Estado getEstado() {
+		return estado;
+	}
+
+	public void setEstado(Estado estado) {
+		this.estado = estado;
+	}
+
+	public Date getDataCriacao() {
+		return dataCriacao;
+	}
+
+	public void setDataCriacao(Date dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
+
+	public Date getDataAtualizacao() {
+		return dataAtualizacao;
+	}
+
+	public void setDataAtualizacao(Date dataAtualizacao) {
+		this.dataAtualizacao = dataAtualizacao;
+	}
+}
